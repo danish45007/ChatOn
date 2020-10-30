f1652c664cba84ecbc53110764d43924e4f48621
danish45007 <danish45007@gmail.com>
2020-10-30 16:53:00 +0530

    MenuBar created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import MenuBar from './components/MenuBar';
+import Register from './components/Register';
+import { Container } from 'semantic-ui-react';
 
 function App() {
-	return <div className="App">Heloo</div>;
+	return (
+		<BrowserRouter>
+			<Container>
+				<MenuBar />
+				<Route exact path="/" component={Home} />
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/register" component={Register} />
+			</Container>
+		</BrowserRouter>
+	);
 }
 
 export default App;
